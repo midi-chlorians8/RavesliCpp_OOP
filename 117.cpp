e8bafc50f732d0aff7991238a5c40dd61aef26d2
midi-chlorians8 <ilya.romanovskiy.2015@mail.ru>
2020-12-19 15:03:22 +0300

    add

--- 117.cpp
+++ 117.cpp
@@ -11,7 +11,7 @@ std::uint8_t m_blue ;
 std::uint8_t m_alpha ;
 
 public:
-  RGBA(std::uint8_t red, std::uint8_t green, std::uint8_t blue, std::uint8_t m_alpha=200) // на этой строке уже и так много чего,
+  RGBA(std::uint8_t red=1, std::uint8_t green=1, std::uint8_t blue=1, std::uint8_t m_alpha=200) // на этой строке уже и так много чего,
         : m_red(red), // поэтому выделяем каждому инициализатору отдельную строку, не забывая о запятой в конце
         m_green(green),
         m_blue(blue),
@@ -19,12 +19,20 @@ public:
         {
 
         }
+        void print()
+	{
+		std::cout << "r=" << static_cast<int>(m_red) <<
+			" g=" << static_cast<int>(m_green) <<
+			" b=" << static_cast<int>(m_blue) <<
+			" a=" << static_cast<int>(m_alpha) << '\n';
+	}
+
 };
 
 
 int main(){
- 	//RGBA color(0, 135, 135,4);
-	//color.print();
+ 	RGBA color(0, 135, 135,4);
+	color.print();
  
      return 0;
 }
